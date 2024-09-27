d12edd0b6151ff483895621bc2fd11b2dea1bc54
鹿又笑 <3062840335@qq.com>
2024-09-27 09:37:41 +0800

    fix:修复题库题目关系接口映射问题

--- src/main/java/com/luyouxiao/mianshixing/service/QuestionBankQuestionService.java
+++ src/main/java/com/luyouxiao/mianshixing/service/QuestionBankQuestionService.java
@@ -5,9 +5,12 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.luyouxiao.mianshixing.model.dto.questionBankQuestion.QuestionBankQuestionQueryRequest;
 import com.luyouxiao.mianshixing.model.entity.QuestionBankQuestion;
+import com.luyouxiao.mianshixing.model.entity.User;
 import com.luyouxiao.mianshixing.model.vo.QuestionBankQuestionVO;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * 题库题目关系服务
@@ -48,4 +51,29 @@ public interface QuestionBankQuestionService extends IService<QuestionBankQuesti
      * @return
      */
     Page<QuestionBankQuestionVO> getQuestionBankQuestionVOPage(Page<QuestionBankQuestion> questionBankQuestionPage, HttpServletRequest request);
+
+    /**
+     * 批量添加题目到题库
+     * @param questionIdList
+     * @param questionBankId
+     * @param loginUser
+     */
+    @Transactional(rollbackFor = Exception.class)
+    void batchAddQuestionsToBank(List<Long> questionIdList, Long questionBankId, User loginUser);
+
+    /**
+     * 批量添加题目到题库（事务，仅供内部调用）
+     *
+     * @param questionBankQuestions
+     */
+    @Transactional(rollbackFor = Exception.class)
+    void batchAddQuestionsToBankInner(List<QuestionBankQuestion> questionBankQuestions);
+
+    /**
+     * 批量从题库中移除题目
+     * @param questionIdList
+     * @param questionBankId
+     */
+    @Transactional(rollbackFor = Exception.class)
+    void batchRemoveQuestionsFromBank(List<Long> questionIdList, Long questionBankId);
 }
